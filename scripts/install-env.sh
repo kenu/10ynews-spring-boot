d684e09e71fc5391110f1262f05d1678e3de7446
platanus <platanus@canxan.com>
2021-09-29 00:10:02 +0900

    feat: Add scripts

---
+++ scripts/install-env.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+GITHUB=https://github.com/platanus-kr/10ynews-spring-boot
+REPOSITORY=~/git/10ynews-spring-boot
+
+## install git, java
+sudo amazon-linux-extras install -y java-openjdk11
+sudo yum install -y git
+
+## git clone
+mkdir ~/git
+cd ~/git
+git clone $GITHUB
+
+## chmod +x scripts
+#cp $REPOSITORY/scripts/deploy-10ynews.sh ~
+#chmod +x ~/deploy-10ynews.sh
+cd 10ynews-spring-boot
+sh gradlew build
+
+JAR_NAME=$(ls $REPOSITORY/build/libs | grep jar | head -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+sleep 10
+
+
+## install nginx
+sudo amazon-linux-extras install -y nginx1.12
+sudo systemctl enable nginx
+
+## replace configuration
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
+sudo sh -c "sed '38,87d' /etc/nginx/nginx.conf.orig > /etc/nginx/nginx.conf"
+cd /etc/nginx/conf.d
+sudo curl -O ~~~~~~~~~~~~~~~~~
+sleep 2
+sudo systemctl start nginx
